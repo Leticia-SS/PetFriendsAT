b1de91bb28da5cbfff762a933897038503caf43b
Leticia-SS <leticia.saraiva013@gmail.com>
2026-06-24 19:48:47 -0300

    comitando targets para rodar o docker

---
+++ almoxarifado-service/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO reserva_estoque (pedido_id, produto_id, quantidade, status, corredor, prateleira, posicao)
+VALUES (1, 101, 50, 'RESERVADO', 'A', '3', '2');
+
+INSERT INTO reserva_estoque (pedido_id, produto_id, quantidade, status, corredor, prateleira, posicao)
+VALUES (2, 102, 10, 'RESERVADO', 'B', '1', '5');
