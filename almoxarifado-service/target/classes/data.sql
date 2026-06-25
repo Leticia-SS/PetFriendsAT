988346b08d92a925ebb489ffc888e599e25dcfa5
Leticia-SS <leticia.saraiva013@gmail.com>
2026-06-24 21:43:25 -0300

    add kafka docker

--- almoxarifado-service/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO reserva_estoque (pedido_id, produto_id, quantidade, status, corredor, prateleira, posicao)
-VALUES (1, 101, 50, 'RESERVADO', 'A', '3', '2');
-
-INSERT INTO reserva_estoque (pedido_id, produto_id, quantidade, status, corredor, prateleira, posicao)
-VALUES (2, 102, 10, 'RESERVADO', 'B', '1', '5');
